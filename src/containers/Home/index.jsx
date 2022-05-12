a06bb82e732cdcab2ccd63a971efc6ae47d67eac
Asrorjon17 <asrorjonrahimjonov17@gmail.com>
2022-05-12 13:54:53 +0500

    react admin panel responsive

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../../pages/Dashboard";
+import Register from "../../pages/Blank";
+import Cards from "../../components/Cards";
+import Buttons from "../../components/Buttons";
+import { Box, Grid } from "@mui/material";
+export default () => (
+	<div>
+		<Dashboard />
+		{/* <Buttons /> */}
+		{/* <Cards /> */}
+		{/* <Register /> */}
+	</div>
+);
